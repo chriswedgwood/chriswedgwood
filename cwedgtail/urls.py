2f42e2f0f03d9556c932c3777b8aa9dd55808e41
Chris Wedgwood <wedgemail@gmail.com>
2020-04-24 17:54:56 +0100

    started graphing stuff

--- cwedgtail/urls.py
+++ cwedgtail/urls.py
@@ -7,6 +7,7 @@ from wagtail.documents import urls as wagtaildocs_urls
 from wagtail.core import urls as wagtail_urls
 
 from search import views as search_views
+from toastmasters import views as toastmaster_views 
 
 urlpatterns = [
     url(r'^django-admin/', admin.site.urls),
@@ -15,6 +16,11 @@ urlpatterns = [
     url(r'^documents/', include(wagtaildocs_urls)),
 
     url(r'^search/$', search_views.search, name='search'),
+    url(r'^toastiemembers/$', toastmaster_views.index_page, name='toastie-index'),
+    url(r'^toastiemembers/memberdata/$', toastmaster_views.member_summary, name='toastie-member-data'),
+    
+    
+
 
     # For anything not caught by a more specific rule above, hand over to
     # Wagtail's page serving mechanism. This should be the last pattern in
--- toastmasters/views.py
+++ toastmasters/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+import altair as alt
+from django.http import JsonResponse
+from django.shortcuts import render
+from .models import Member
 
 # Create your views here.
+
+
+def member_summary(request):
+    query = Member.objects.all().values()
+    data = alt.Data(values=list(query))
+    chart_obj = alt.Chart(data).mark_bar().encode(
+
+    )
+    return JsonResponse(chart_obj,safe=False)
+
+
+def index_page(request):
+    return render(request,"index_page.html")
