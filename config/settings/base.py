5d2c99898d4610b69d90b28461bce1b27de8351f
Chris Wedgwood <wedgemail@gmail.com>
2021-04-09 23:56:04 +0100

    mw

--- config/settings/base.py
+++ config/settings/base.py
@@ -67,7 +67,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-
+    'django.contrib.sites.middleware.CurrentSiteMiddleware',
     'wagtail.contrib.redirects.middleware.RedirectMiddleware',
 
 ]
