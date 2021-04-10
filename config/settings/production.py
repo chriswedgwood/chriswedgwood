de9e7bb56e176c48c871efd4b7062cc0d0136d36
Chris Wedgwood <wedgemail@gmail.com>
2021-04-10 09:15:48 +0100

    db

--- config/settings/production.py
+++ config/settings/production.py
@@ -11,7 +11,7 @@ except ImportError:
 
 
 SECRET_KEY = env('DJANGO_SECRET_KEY')
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ['chriswedgwood.com','www.chriswedgwood.com',] 
 
 ############################################
