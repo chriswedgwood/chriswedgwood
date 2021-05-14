7acc489025aa15cfe9c06018f1f7bbda43f50df8
Chris Wedgwood <wedgemail@gmail.com>
2021-05-14 15:49:35 +0100

    test

--- config/settings/base.py
+++ config/settings/base.py
@@ -145,7 +145,8 @@ MEDIA_URL = "/media/"
 
 WAGTAIL_SITE_NAME = "cwedgtail"
 
-# Base URL to use when referring to full URLs within the Wagtail admin backend -
+# Base URL to use when referring to full URLs within
+# the Wagtail admin backend -
 # e.g. in notification emails. Don't include '/admin' or a trailing slash
 BASE_URL = "http://example.com"
 
